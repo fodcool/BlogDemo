a781eeea7a11955059c9f5058b9b6ebbc7aff015
yuancheng <330469102@qq.com>
2016-12-12 16:17:48 +0800

    修改了路由 添加了gem 修改了User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
+
   resources :articles do
     collection do
       get :welcome
     end
   end
 
-  devise_for :users do
-    get "/signup", :to => "devise/registrations#new", :as => :signup
-  end
+  devise_for :users
+
+  resources :users
 
   root to: "articles#welcome"
 end
